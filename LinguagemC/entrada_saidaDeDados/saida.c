fc84c4b3ceb04041f110b2c1c5ad5322204aebc1
AurelioAlves4002 <aurelio.alves2004@gmail.com>
2025-09-08 15:57:04 -0300

    Entrada e Saída de Dados

---
+++ LinguagemC/entrada_saidaDeDados/saida.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ 
+// exemplo com printf
+
+int main() {
+    int idade;
+    printf("Digite sua idade: \n");
+    scanf("%d", &idade);
+    printf("Sua idade é: %d\n", idade);
+    return 0;
+}
