86bb90917c151d218d2eb2c1c7fabb8e7f79f482
AurelioAlves4002 <aurelio.alves2004@gmail.com>
2025-09-20 14:31:07 -0300

    Exemplo de variáveis inteiras

---
+++ LinguagemC/manipulacaoCoversaoDeTiposDeDados/variaveisInteiras.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+
+int main() {
+    int a = 10;          // Variável inteira
+    int b = 3;           // Outra variável inteira
+    int soma = a + b;    // Soma de inteiros
+    int diferenca = a - b; // Diferença de inteiros
+    int produto = a * b; // Produto de inteiros
+    int quociente = a / b; // Quociente de inteiros
+
+    printf("Soma: %d\n", soma);
+    printf("Diferença: %d\n", diferenca);
+    printf("Produto: %d\n", produto);
+    printf("Quociente: %d\n", quociente);
+
+    return 0;
+}
