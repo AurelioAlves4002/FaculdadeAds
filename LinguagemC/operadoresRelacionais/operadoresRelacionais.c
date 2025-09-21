7db4e505eacfdde0ff76d9c0b14e65396adf094e
AurelioAlves4002 <aurelio.alves2004@gmail.com>
2025-09-21 17:17:18 -0300

    Exemplo de operadores relacionais

---
+++ LinguagemC/operadoresRelacionais/operadoresRelacionais.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main () {
+
+    int a = 10;
+    int b = 20;
+
+    printf("a < b: %d\n", a < b);   // Menor que
+    printf("a > b: %d\n", a > b);   // Maior que
+    printf("a <= b: %d\n", a <= b); // Menor ou igual a
+    printf("a >= b: %d\n", a >= b); // Maior ou igual a
+    printf("a == b: %d\n", a == b); // Igual a
+    printf("a != b: %d\n", a != b); // Diferente de
+
+    return 0;
+}
