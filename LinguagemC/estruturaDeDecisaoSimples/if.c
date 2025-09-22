373e4795c19ac5d55e853700b74bb1fe9f0b23ea
AurelioAlves4002 <aurelio.alves2004@gmail.com>
2025-09-22 14:51:43 -0300

    Exemplo simples do uso do if

---
+++ LinguagemC/estruturaDeDecisaoSimples/if.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    // Exemplo simples de estrutura de decisão if
+
+    int numero1 = 10;
+    int numero2 = 20;
+
+    if (numero1 < numero2) {
+        printf("Numero1 e menor que Numero2\n");
+    }
+
+    return 0;
+}
