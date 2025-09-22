3aa0144fd4920a3aafb32707cc178071f3ac70e2
AurelioAlves4002 <aurelio.alves2004@gmail.com>
2025-09-22 14:58:14 -0300

    Exemplo simples do uso de if e else

---
+++ LinguagemC/estruturaDeDecisaoComposta/if-else.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    // Exemplo simples de estrutura de decisão if-else
+    int numero = 10;
+    if (numero % 2 == 0) {
+        printf("O numero %d e par.\n", numero);
+    } else {
+        printf("O numero %d e impar.\n", numero);
+    }
+    
+    return 0;
+}
